7a160f0f847cff9e3ddff443190809b4b476f999
Juliamello08 <juliainline@gmail.com>
2020-06-15 17:53:08 -0300

    Criando menu login/registro

--- JogoForca/src/com/github/juliamello8/classes/Login.java
+++ JogoForca/src/com/github/juliamello8/classes/Login.java
@@ -2,6 +2,10 @@ package com.github.juliamello8.classes;
 
 public class Login extends Registrar {
 	
+	public void realizarLogin() {
+		System.out.print(" \nAQUI VEM A LOGICA COM BANCO DE DADOS\n");			
+	}
+	
 	public void verLogin() {
 		System.out.print("Seu login é: " + getLogin());
 		System.out.print("\nSua senha é: " + getSenha());
